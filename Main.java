4fb08cbdd9775c3fedb2b4761dff4b9db2419588
Alisa-min <1731604140@qq.com>
2020-03-15 18:08:42 +0800

    Test

---
+++ Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+            byte var2 = 10;
+            byte var3 = 20;
+            System.out.println("a=" + var3);
+            System.out.println("b=" + var2);
+        
+
+    }
+}
